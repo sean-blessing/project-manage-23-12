5a43be99056820e700e2ed01d4165503b3f25198
Sean Blessing <sblessing@maxtrain.com>
2023-12-11 16:42:49 -0500

    init commit

---
+++ src/app/projects/projects-container/projects-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-container',
+  templateUrl: './projects-container.component.html',
+  styleUrl: './projects-container.component.css'
+})
+export class ProjectsContainerComponent {
+
+}
